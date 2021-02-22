bbd7a53c2e2cdd65a71ee806a7b9c3b6b24f5819
robert1611 <robert1611@gmail.com>
2021-02-22 07:41:04 -0600

    swapped token for testing

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,12 +1,14 @@
-const token = '' // TODO: Add API Access Key
+const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJvd25lciI6ImQ3Mjc5ZDMwMGU3NjFjNDJhZTgwNTA5NzZiYjk0MzA1ZjhiMGUzNTMxNmY3ZDU3MSIsImFjY2Vzc190aW1lIjoiXCIyMDIxLTAyLTIwIDA2OjQzOjA2LjMxNzY3MlwiIn0.dakafkB0Pm51IxjAvanyiaXgiTp28d8TjMSbk5ZtJ8I' // TODO: Add API Access Key
+
+const heroku_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJvd25lciI6IjliMjgzZjkzOTMzNWUyNTcyNTA4OTRlNGY2OGIyNTg5NGMxZWIzNDA3YWMyOTQ3ZiIsImFjY2Vzc190aW1lIjoiXCIyMDIxLTAyLTIyIDEzOjM1OjI2LjE1OTcyMFwiIn0.HWmvvF_n4zSzV1M2UIYNSa1H7qIsww5xdk0auODZ2As'
 
 export const server_calls = {
     get: async () => {
-        const response = await fetch(`/marvels`,{
+        const response = await fetch(`https://marvel-rd.herokuapp.com/marvels`,{
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
-                'x-access-token': `Bearer ${token}`
+                'x-access-token': `Bearer ${heroku_token}`
             }
         });
 
@@ -17,11 +19,11 @@ export const server_calls = {
         return await response.json()
     },    
 		delete: async (id:string) => {
-        const response = await fetch(`/marvels/${id}`,{
+        const response = await fetch(`https://marvel-rd.herokuapp.com/marvels/${id}`,{
             method: 'DELETE',
             headers: {
                 'Content-Type': 'application/json',
-                'x-access-token': `Bearer ${token}`
+                'x-access-token': `Bearer ${heroku_token}`
             }
         });
 
@@ -32,11 +34,11 @@ export const server_calls = {
         return await response.json()
     },
 		update: async (id:string, data:any = {}) => {
-        const response = await fetch(`/marvels/${id}`, {
+        const response = await fetch(`https://marvel-rd.herokuapp.com/marvels/${id}`, {
             method: 'POST', // PUT also works here
             headers: {
                 'Content-Type': 'application/json',
-                'x-access-token': `Bearer ${token}`
+                'x-access-token': `Bearer ${heroku_token}`
             },
             body: JSON.stringify(data)
         });
@@ -47,13 +49,13 @@ export const server_calls = {
         return await response.json()
     },
 		create: async (data:any = {}) => {
-        const response = await fetch(`/marvels`,{
-            method: 'POST',
-            headers: {
+            const response = await fetch(`https://marvel-rd.herokuapp.com/marvels`,{
+                method: 'POST',
+                headers: {
                 'Content-Type': 'application/json',
-                'x-access-token': `Bearer ${token}`
-            },
-            body: JSON.stringify(data)
+                'x-access-token': `Bearer ${heroku_token}`
+                },
+                 body: JSON.stringify(data)
         });
         if(!response.ok){
             throw new Error('Failed to create new data on server')
